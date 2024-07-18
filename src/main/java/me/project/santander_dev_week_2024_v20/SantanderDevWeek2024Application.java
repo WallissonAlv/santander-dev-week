8235bb870ed843b2121e00884b5eea11a90a3017
WallissonAlv <wallissonlima008@gmail.com>
2024-07-18 13:46:03 -0300

    Initial commit

---
+++ src/main/java/me/project/santander_dev_week_2024_v20/SantanderDevWeek2024Application.java
@@ -0,0 +1,13 @@
+package me.project.santander_dev_week_2024_v20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/project/santander_dev_week_2024_v20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.project.santander_dev_week_2024_v20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
